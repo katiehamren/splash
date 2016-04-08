4fbda309571c8544e24263cadab789560e79f2bd
Katie Hamren <khamren@Bast.local>
2016-04-08 10:38:54 -0700

    initializing splash repo with Shablona framework

--- shablona/version.py
+++ shablona/version.py
@@ -25,52 +25,49 @@ CLASSIFIERS = ["Development Status :: 3 - Alpha",
                "Topic :: Scientific/Engineering"]
 
 # Description should be a one-liner:
-description = "shablona: a template for small scientific Python projects"
+description = "splash: a module for accessing and formatting SPLASH data"
 # Long description will go up on the pypi page
 long_description = """
 
-Shablona
+splash
 ========
-Shablona is a template project for small scientific Python projects.
+splash is a module for creating databases of data from the SPLASH survey
 
-It contains software implementations of an analysis of some simple data, but
-more importantly, it contains infrastructure for testing, documentation,
-continuous integration and deployment, which can be easily adapted
-to use in other projects.
+To get started you will need to contact Prof. Puragra GuhaThakurta at UC Santa Cruz
+for access to the SPLASH data.
 
-To get started using these components in your own software, please go to the
+To get started with this code, head to the
 repository README_.
 
 .. _README: https://github.com/uwescience/shablona/blob/master/README.md
 
 License
 =======
-``shablona`` is licensed under the terms of the MIT license. See the file
+``splash`` is licensed under the terms of the MIT license. See the file
 "LICENSE" for information on the history of this software, terms & conditions
 for usage, and a DISCLAIMER OF ALL WARRANTIES.
 
 All trademarks referenced herein are property of their respective holders.
 
-Copyright (c) 2015--, Ariel Rokem, The University of Washington
-eScience Institute.
+Copyright (c) 2016--, Katherine Hamren, UC Santa Cruz
 """
 
-NAME = "shablona"
-MAINTAINER = "Ariel Rokem"
-MAINTAINER_EMAIL = "arokem@gmail.com"
+NAME = "splash"
+MAINTAINER = "Katherine Hamren"
+MAINTAINER_EMAIL = "katherine.hamren@gmail.com"
 DESCRIPTION = description
 LONG_DESCRIPTION = long_description
-URL = "http://github.com/uwescience/shablona"
+URL = "http://github.com/katie.hamren/splash"
 DOWNLOAD_URL = ""
 LICENSE = "MIT"
-AUTHOR = "Ariel Rokem"
-AUTHOR_EMAIL = "arokem@gmail.com"
+AUTHOR = "Katherine Hamren"
+AUTHOR_EMAIL = "katherine.hamren@gmail.com"
 PLATFORMS = "OS Independent"
 MAJOR = _version_major
 MINOR = _version_minor
 MICRO = _version_micro
 VERSION = __version__
-PACKAGES = ['shablona',
-            'shablona.tests']
-PACKAGE_DATA = {'shablona': [pjoin('data', '*')]}
-REQUIRES = ["numpy"]
+PACKAGES = ['splash',
+            'splash.tests']
+PACKAGE_DATA = {'splash': [pjoin('data', '*')]}
+REQUIRES = ["astropy","numpy"]
